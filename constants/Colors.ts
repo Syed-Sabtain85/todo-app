82c39272307a29dec7a96a3b40bfa20413d60b97
Syed Sabtain <sabtain@xcellmobi.com>
2025-12-31 13:07:42 +0500

    theme

---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+export const Colors = {
+light:{
+text: '#000000',
+background: '#ffffff',
+icon : '#000000',
+button: '#6200ee',
+buttonText: '#ffffff',
+inputBackground: '#f5f5f5',
+inputText: '#000000',
+placeHolderText: '#6200ee'
+},
+dark:{
+text: '#ffffffff',
+background: '#000000ff',
+icon : '#ffffffff',
+button: '#a185c9ff',
+buttonText: '#000000ff',
+inputBackground: '#f5f5f5',
+inputText: '#000000',
+placeHolderText: '#a185c9ff'
+}
+};
---
+++ context/ThemeContext.js
@@ -0,0 +1,10 @@
+import {Children, createContext} from 'react'
+import { Appearance } from 'react-native/types_generated/index'
+import {Colors} from '../constants/Colors'
+
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({childern}) =>{
+
+}
